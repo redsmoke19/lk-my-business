a3ad56a9fc786c0a604b95a22416c3e216aa403f
Oleg Tsyganov <redsmoke19@gmail.com>
2022-02-26 22:27:26 +0400

    Доработал полностью всю верстку и сверстал новые компоненты

---
+++ source/js/init-accordions.js
@@ -0,0 +1,11 @@
+import { Accordions } from './vendor/accordions';
+
+let accordions;
+
+const initAccordions = () => {
+  accordions = new Accordions();
+  // Используйте в разработке экспортируемую переменную accordions, window сделан для бэкэнда
+  window.accordions = accordions;
+};
+
+export { initAccordions, accordions };
--- source/js/main.js
+++ source/js/main.js
@@ -2,18 +2,20 @@ import datePicker from './date-picker';
 import user from './user';
 // import user from './user';
 import about from './about';
-import faq from './faq';
+// import faq from './faq';
 import range from './range';
 import form from './form';
 import { initDropZone } from './dropzone';
+import { initAccordions } from './init-accordions';
 
 document.addEventListener('DOMContentLoaded', () => {
   // user();
   // about();
   datePicker();
   user();
-  faq();
+  // faq();
   range();
   form();
   initDropZone();
+  initAccordions();
 });
