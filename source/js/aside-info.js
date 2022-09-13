492ee2a2af3733509a495a7b9c70ed2e2d3fc3bb
Oleg Tsyganov <redsmoke19@gmail.com>
2022-09-13 22:37:17 +0400

    fix: правки по верстке

--- source/js/aside-info.js
+++ source/js/aside-info.js
@@ -3,16 +3,25 @@ const breakpointTablet = window.matchMedia('(max-width: 1023px)');
 const getAsideInfo = () => {
   const asideInfo = document.querySelector('.aside-info');
   const tabs = document.querySelector('.tabs-nav__header');
+  const wrapper = document.querySelector('.about__wrapper');
 
   if (!asideInfo) {
     return;
   }
 
+  const resizeHeight = () => {
+    wrapper.style.height = `${asideInfo.clientHeight} - ${tabs.clientHeight}px`;
+  };
+
   const breakpointChecker = () => {
     if (breakpointTablet.matches) {
       asideInfo.style.top = 'unset';
+      wrapper.style.minHeight = 'unset';
     } else {
       asideInfo.style.top = `-${tabs.clientHeight}px`;
+      wrapper.style.minHeight = `${asideInfo.clientHeight - tabs.clientHeight}px`;
+
+      window.addEventListener('resize', resizeHeight);
     }
   };
 
